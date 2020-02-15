2e1aea85944183136b41e83378a59219f8e1db4d
barutkin <barutkin@gmail.com>
2020-02-15 16:07:11 +0700

    add Cli.php with the use of php-cli-tools

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function cli\prompt;
+use function cli\line;
+
+function run()
+{
+	$name = prompt('May I have your name?');
+	line("Hello, {$name}!");
+}
+

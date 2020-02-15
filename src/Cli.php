1e1005d85588d651bd5e9f9dbb9ec096347547a6
barutkin <barutkin@gmail.com>
2020-02-16 02:08:10 +0700

    add phpcs

--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,6 @@ use function cli\line;
 
 function run()
 {
-	$name = prompt('May I have your name?');
-	line("Hello, {$name}!");
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}!");
 }
-

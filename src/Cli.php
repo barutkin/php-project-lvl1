cc8c97110a2ef156446e0e223a4b54d44b35f058
barutkin <barutkin@gmail.com>
2020-02-16 18:21:54 +0700

    implement printGreeting()

--- src/Cli.php
+++ src/Cli.php
@@ -5,6 +5,11 @@ namespace BrainGames\Cli;
 use function cli\prompt;
 use function cli\line;
 
+function printGreeting()
+{
+    print_r("Welcome to the Brain Games!\n\n");
+}
+
 function run()
 {
     $name = prompt('May I have your name?');
